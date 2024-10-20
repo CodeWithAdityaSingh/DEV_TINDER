0ce9942cb2c22bc8f0036de92baa4a5a63ae8545
Aditya Singh <adityasingh161020003@gmail.com>
2024-10-20 11:35:45 +0530

    Learning about get,post,patch,delete,put

--- src/app.js
+++ src/app.js
@@ -4,13 +4,22 @@ let app = express()
 
 
 
-app.use('/test',(req,res)=>{
-    res.send('Hellow this is port 1000')
+
+app.post('/user',(req,res)=>{
+    res.send({Name : "Aditya Singh",Qualification : "Graduate"})
 })
 
+app.get('/user',(req,res)=>{
+   res.send('i am port 1000')
+
+})
+
+app.delete('/user',(req,res)=>{
+   res.send('Deleted Successfully')
+})
 
 app.listen(1000,()=>{
 
-       console.log('Listning on Port 1000');
+       console.log('Listninig on Port 1000');
        
 })
